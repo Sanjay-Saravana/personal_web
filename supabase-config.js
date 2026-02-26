e706943a82146b7ffeaa817ada08c7ad18d04aa0
Sanjay-Saravana <softdevsanjay@gmail.com>
2026-02-26 12:39:08 +0530

    add

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,3 @@
-// Copy this file to `supabase-config.js` and fill real values.
-// This file should NOT be committed with real keys.
 window.__SUPABASE_CONFIG = {
   url: 'https://uxiitzgyvyxtsevgevjw.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4aWl0emd5dnl4dHNldmdldmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwODM1NDAsImV4cCI6MjA4NzY1OTU0MH0.KwHmV8_b5Aoeab9wNO9t0eyfVHOC3N63DBgghtd77do',
